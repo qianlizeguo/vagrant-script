7a2b0c8458655939aec92afd109d2ac0cdf61391
qianlizeguo <1723877247@qq.com>
2019-05-13 19:30:31 +0800

    add some script

--- start.sh
+++ start.sh
@@ -15,3 +15,16 @@ docker run -d  -p 2222:22 \
                --name gitlab  \
                gitlab/gitlab-ce
 
+echo ">>>>  启动mysql"
+docker run --name mysql -d --rm \
+            -v /docker/mysql/conf:/etc/mysql/conf.d \
+            -v /docker/mysql/data:/var/lib/mysql \
+            --privileged=true \
+            -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root mysql
+
+echo ">>>> 启动redis"
+docker run --name redis -d \
+            -p 6379:6379 \
+            -v /docker/redis/data:/data \
+            --privileged=true redis \
+            redis-server --appendonly yes
