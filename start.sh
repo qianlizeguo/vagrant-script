faa15595d30c40a2ac5afe90490cd05fa364fa3f
qianlizeguo <1723877247@qq.com>
2019-05-14 13:36:56 +0800

    add some script

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ echo ">>> 开始启动docker.service "
 service docker start
 
 echo ">>> 开始启动gitlab "
-docker run -d  -p 2222:22 \
+docker rm gitlab
+docker run -d  -p 8222:22 \
                -p 8081:80 \
                -p 8443:443 \
                -v /docker/gitlab/config:/etc/gitlab \
@@ -16,6 +17,7 @@ docker run -d  -p 2222:22 \
                gitlab/gitlab-ce
 
 echo ">>>>  启动mysql"
+docker rm mysql
 docker run --name mysql -d --rm \
             -v /docker/mysql/conf:/etc/mysql/conf.d \
             -v /docker/mysql/data:/var/lib/mysql \
@@ -23,6 +25,7 @@ docker run --name mysql -d --rm \
             -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root mysql
 
 echo ">>>> 启动redis"
+docker rm redis
 docker run --name redis -d \
             -p 6379:6379 \
             -v /docker/redis/data:/data \
